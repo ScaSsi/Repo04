a96b9785635a826a8f7666e556f7990f3246969a
Avujcic1 <avujcic1@etf.unsa.ba>
2022-03-31 14:27:31 +0200

    Decimal numbers

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,7 +1,7 @@
 function main(){
     // Only change code below this line
     var myDecimal = 8.2;
-    var myDecimalE = 2.54e-3
+    var myDecimalE = 2.54e-3;
     // Only change code above this line
     return {
         myDecimal,
